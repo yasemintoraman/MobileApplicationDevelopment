04bcae9fb7dfae354374184bd6712a61f527821d
U190709023 <yasemintoraman@posta.mu.edu.tr>
2022-10-09 11:29:34 +0300

    lab3 completed

---
+++ Week3-Mytagram/app/src/main/java/com/yasemintoraman/week3_mytagram/Post.java
@@ -0,0 +1,34 @@
+package com.yasemintoraman.week3_mytagram;
+
+import android.graphics.Bitmap;
+import android.location.Location;
+
+public class Post {
+    private Bitmap image;
+    private Location location;
+    private String message;
+
+    public Bitmap getImage() {
+        return image;
+    }
+
+    public void setImage(Bitmap image) {
+        this.image = image;
+    }
+
+    public Location getLocation() {
+        return location;
+    }
+
+    public void setLocation(Location location) {
+        this.location = location;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
---
+++ Week3-Mytagram/app/src/main/java/com/yasemintoraman/week3_mytagram/PostActivity.java
@@ -0,0 +1,74 @@
+package com.yasemintoraman.week3_mytagram;
+
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.media.Image;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+
+public class PostActivity extends AppCompatActivity {
+
+    ImageView img;
+    static final int CAPTURE_IMAGE = 0;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_post);
+
+        img = (ImageView) findViewById(R.id.imageView);
+        img.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                if (intent.resolveActivity(getPackageManager()) != null){
+                    startActivityForResult(intent, CAPTURE_IMAGE);
+                }
+            }
+        });
+
+        EditText txtMsg = (EditText)findViewById(R.id.txtMessage);
+        ImageButton btnOk = (ImageButton) findViewById(R.id.btnOk);
+        btnOk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                Bundle bundle = new Bundle();
+                bundle.putCharSequence("msg",txtMsg.getText());
+                bundle.putParcelable("bitmap",((BitmapDrawable)img.getDrawable()).getBitmap());
+                intent.putExtras(bundle);
+                setResult(Activity.RESULT_OK,intent);
+                finish();
+            }
+        });
+        ImageButton btnCancel = (ImageButton) findViewById(R.id.btnCancel);
+        btnCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setResult(Activity.RESULT_CANCELED);
+                finish();
+            }
+        });
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode,resultCode,data);
+        if (requestCode == CAPTURE_IMAGE && resultCode == Activity.RESULT_OK){
+            Bundle bundle = data.getExtras();
+            Bitmap image = (Bitmap) bundle.get("data");
+            img.setImageBitmap(image);
+        }
+    }
+}
